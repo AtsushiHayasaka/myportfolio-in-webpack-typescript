449f2cff2b9af85f2c1d7809d7e6eec1bf6d9405
AtsushiHayasaka <a.hayusaka0614@gmail.com>
2021-09-16 11:43:10 +0900

    webpack-dev-serverを使用するとCannot GET/と出て詰んでます。

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const root = document.getElementById('root');
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = 'Hello webpack!';
+  return element;
+}
+document.body.appendChild(component());
